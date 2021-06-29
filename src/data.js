ecc07583cbb283924ed07820b99176bdbddd0c31
Stacy Grenier <sngrenier@gmail.com>
2021-06-29 10:31:46 -0700

    stashing-new-updates

--- src/data.js
+++ src/data.js
@@ -416,5 +416,483 @@
 //     "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
 //   inCart: false,
 //   count: 0,
-//   total: 0,
-// };
+// //   total: 0,
+// // };
+// export const circuits = [
+//     {
+//       id: 1,
+//       title: "Full Body",
+//       subtite: "RESISTANCE",
+//       mainImg: "https://cdn.statically.io/img/wallpapercart.com/wp-content/uploads/2019/09/fitness-desktop-wallpaper-scaled.jpg?quality=100&f=auto",
+//       info: "4 Circuits  |  8 Exercises  |  28-40 min",
+//     },
+//     {
+//       id: 2,
+//       title: "Abs",
+//       subtite: "RESISTANCE",
+//       mainImg: "https://www.wallpaperup.com/uploads/wallpapers/2017/02/23/1079361/9a43ad7af0b01468a17a63a5681e9a91-700.jpg",
+//       info: "4 Circuits  |  8 Exercises  |  28-40 min",
+//     },
+//     {
+//       id: 3,
+//       title: "Legs",
+//       subtite: "RESISTANCE",
+//       mainImg: "https://rawcorporatehealth.com/wp-content/uploads/2017/02/fitness-gym-women-exercising.jpg",
+//       info: "4 Circuits  |  8 Exercises  |  28-40 min",
+//     },
+//     {
+//       id: 4,
+//       title: "Arms + Abs",
+//       subtite: "RESISTANCE",
+//       mainImg: "https://i.cdnraw.com/previews/downloads/d/8/w/p-woman-lifting-weights-8wvIBriisC-1.jpg",
+//       info: "4 Circuits  |  8 Exercises  |  28-40 min",
+//     },
+//     {
+//       id: 5,
+//       title: "600 Rep Challenge",
+//       subtite: "RESISTANCE",
+//       mainImg: "https://i.pinimg.com/originals/0c/8e/b8/0c8eb8bb610ba7f4970a1cef39459f11.jpg",
+//       info: "4 Circuits  |  8 Exercises  |  28-40 min",
+//     }
+//   ];
+  
+
+//   // IMPORTANT NOTE: The circuits are in order already - they are based on move and outfit (I wanted to make sure the outfit was the same for all 4 moves in the circuit)
+//   // 
+//   export const circuitMove = [
+//     {
+//         id: 1,
+//         title: "Burpee",
+//         img: "",
+//         gif:
+//           "https://i.pinimg.com/originals/dc/c9/f2/dcc9f23e69530587d9dba15eaaec5dee.gif",
+//         reps: "10 Reps",
+//         info:"",
+//       },
+//       {
+//           id: 2,
+//           title: "V-Sit Leg Extensions",
+//           img: "",
+//           gif:
+//             "https://i.pinimg.com/originals/49/16/a4/4916a4205cc580f95db6e20a345f002f.gif",
+//           reps: "40 Reps (20 Per Side)",
+//           info:"",
+//         },
+//         {
+//           id: 3,
+//           title: "Star Straight-Leg Jackknife",
+//           img: "",
+//           gif:
+//             "https://i.pinimg.com/originals/b7/ae/01/b7ae012431988c83c4c117375f2b1028.gif",
+//           reps: "12 Reps",
+//           info:"",
+//         },
+//         {
+//           id: 4,
+//           title: "Full Extension Kick-Back",
+//           img: "",
+//           gif:
+//             "https://3.bp.blogspot.com/-nMSgJq-WSBw/W7WrPZaJQWI/AAAAAAAAYTU/wYYCjfaydc04vazrqYt8xEA9JR1ujsQKQCKgBGAs/s1600/Kat-Forearm-Plank-Booty-Kickbacks.gif",
+//           reps: "15 Reps",
+//           info:"",
+//         },
+//         {
+//             id: 5,
+//             title: "Weighted Plie Lunge & Side Kick",
+//             img: "",
+//             gif:
+//               "https://imagesvc.meredithcorp.io/v3/mm/gif?q=85&c=sc&poi=face&w=900&h=900&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F14%2F2017%2F01%2F09%2F010517-tone-it-up-2.gif",
+//             reps: "15 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 6,
+//             title: "Straight Leg Hip Bridge",
+//             img: "",
+//             gif:
+//               "https://media.glamour.com/photos/586d34a63c6164143719aa85/master/w_1600%2Cc_limit/Kat-Single-Leg-Bridge-Dip_Tone_It_Up.gif",
+//             reps: "20 Reps (10 Per Side)",
+//             info:"",
+//           },
+//           {
+//             id: 7,
+//             title: "Weighted Reverse Lunge & Tricep Press",
+//             img: "",
+//             gif:
+//               "https://i.pinimg.com/originals/04/a3/0d/04a30d3847338ab76be264e455b5a544.gif",
+//             reps: "12 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 8,
+//             title: "Weighted Single Leg Dead Lift",
+//             img: "",
+//             gif:
+//               "https://1.bp.blogspot.com/-ij2S4ptCiDI/WscFsN1gPzI/AAAAAAAAK5E/Qklo8sRTvqwsAWiCcPjHWSvxWnlHaTJpQCEwYBhgL/s1600/Kat-DB-Single-Leg-Deadlift-2.gif",
+//             reps: "10 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 9,
+//             title: "Jump Squat",
+//             img: "",
+//             gif:
+//               "https://images.shape.mdpcdn.com/sites/shape.com/files/styles/slide/public/poppin-plyo-jumps-1-.gif",
+//             reps: "10 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 10,
+//             title: "Ab Bikes",
+//             img: "",
+//             gif:
+//               "https://i.pinimg.com/originals/a8/7c/d7/a87cd7734e82bdb5a264268a4afe1116.gif",
+//             reps: "15 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 11,
+//             title: "Plank & Kick-Back",
+//             img: "",
+//             gif:
+//               "https://bornrealist.com/wp-content/uploads/2017/10/Katrina-Single-Leg-Donkey-Kicks-CROPPED-1-_0.gif",
+//             reps: "20 Reps (10 Per Side)",
+//             info:"",
+//           },
+//           {
+//             id: 12,
+//             title: "Running Man Hip Bridge",
+//             img: "",
+//             gif:
+//               "https://4.bp.blogspot.com/-57S8RXhFbko/Wr3Y8qN00dI/AAAAAAAAKUo/MPzPj0H25d4oCnnIY0tq8tSMTyNzgDEWACEwYBhgL/s1600/Katrina-Bridge-March.gif",
+//             reps: "10 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 13,
+//             title: "Weighted Squat & Press",
+//             img: "",
+//             gif:
+//               "https://i.pinimg.com/originals/22/81/04/22810414c00749510ecf14782c5afb93.gif",
+//             reps: "15 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 14,
+//             title: "Lateral Lunge & High-Knees",
+//             img: "",
+//             gif:
+//               "http://cdn.lowgif.com/full/bc333564bb0f4a6b-.gif",
+//             reps: "20 Reps (10 Per Side)",
+//             info:"",
+//           },
+//           {
+//             id: 15,
+//             title: "Weighted Single Leg Dead Lift & Row",
+//             img: "",
+//             gif:
+//               "https://i.pinimg.com/originals/c2/ed/40/c2ed4017360c22036f461c16611d2f70.gif",
+//             reps: "12 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 16,
+//             title: "Weighted Reverse Lunge & Twist",
+//             img: "",
+//             gif:
+//               "https://1.bp.blogspot.com/-_ZeXLa5JiNA/W-fSiaTsuRI/AAAAAAAAZ9A/X107QVdjmeU9v_B0bAgXS0vpTLYsVMgIQCLcBGAs/s1600/Katrina-Kettle-Bell-Lunge-_-Twist-2.gif",
+//             reps: "10 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 17,
+//             title: "Weighted Reverse Lunge & Kick-Back",
+//             img: "",
+//             gif:
+//               "https://imagesvc.meredithcorp.io/v3/mm/gif?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2016%2F10%2F04034844%2Fskyword-image-352307.gif",
+//             reps: "12 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 18,
+//             title: "High Knees",
+//             img: "",
+//             gif:
+//               "https://www.byrdie.com/thmb/b7jVsmB-Yt-IIlWBEavA5O3mt9I=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__posts__239476__best-core-exercises-for-women-239476-1508796350476-main-bcdaaba08bec4724b9af22cf0787b149.gif",
+//             reps: "40 Reps (20 Per Side)",
+//             info:"",
+//           },
+//           {
+//             id: 19,
+//             title: "Weighted Squat",
+//             img: "",
+//             gif:
+//               "https://imagesvc.meredithcorp.io/v3/mm/gif?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2016%2F10%2F04034845%2Fskyword-image-352308.gif",
+//             reps: "12 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 20,
+//             title: "Weighted Russian Twist",
+//             img: "",
+//             gif:
+//               "https://www.byrdie.com/thmb/X6i-SN4uc7oh5OnJRcWW5EC_Ucs=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__posts__239476__best-core-exercises-for-women-239476-1508796422880-main-7e764e0e875a421c957e26a586849ab2.gif",
+//             reps: "30 Reps (15 Per Side)",
+//             info:"",
+//           },
+//           {
+//             id: 21,
+//             title: "Weighted Sumo Squat",
+//             img: "",
+//             gif:
+//               "https://3.bp.blogspot.com/-eWIAkgQhAcc/W77GcRQjDxI/AAAAAAAAYoQ/-7nAUlW-pF8_IRcWLB6KW_3EufLSotaVQCLcBGAs/s1600/Kat-Sumo-Squats-Square_d39a0021-4cbd-4e1c-8c7f-fc0a1fa6437d.gif",
+//             reps: "12 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 22,
+//             title: "Plank & Weighted Reverse Arm Swing",
+//             img: "",
+//             gif:
+//               "https://media1.popsugar-assets.com/files/thumbor/HvUHMLcG6FwrISbgQ_hXjwbKKGw/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/10/16/941/n/1922729/0038b22d3dc940e7_Kat-Plank-Tricep-Kickbacks-Square/i/Plank-Tricep-Kickbacks.gif",
+//             reps: "12 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 23,
+//             title: "Elevated Jump Squat",
+//             img: "",
+//             gif:
+//               "https://4.bp.blogspot.com/-mdPaxfzBP6o/WM6F1mkGauI/AAAAAAAAAIE/T_fZuQ9Z6WEkvj1IRYp_2otcI37uZDbRACLcB/s1600/10.gif",
+//             reps: "10 Reps",
+//             info:"",
+//           },
+//           {
+//             id: 24,
+//             title: "Half Burpee",
+//             img: "",
+//             gif:
+//               "https://www.byrdie.com/thmb/iHgVuvcsoVowTdLgx5WAthGIxB8=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__posts__239476__best-core-exercises-for-women-239476-1508796471963-main-64af4fcda2a54eedbd7c395682c4fcd5.gif",
+//             reps: "15 Reps",
+//             info:"",
+//           }
+    
+// ];
+
+// {
+//     id: 1,
+//     title: "Burpee",
+//     img: "",
+//     gif:
+//       "https://i.pinimg.com/originals/dc/c9/f2/dcc9f23e69530587d9dba15eaaec5dee.gif",
+//     reps: "10 Reps",
+//     info:"",
+//   },
+//   {
+//       id: 2,
+//       title: "V-Sit Leg Extensions",
+//       img: "",
+//       gif:
+//         "https://i.pinimg.com/originals/49/16/a4/4916a4205cc580f95db6e20a345f002f.gif",
+//       reps: "40 Reps (20 Per Side)",
+//       info:"",
+//     },
+//     {
+//       id: 3,
+//       title: "Star Straight-Leg Jackknife",
+//       img: "",
+//       gif:
+//         "https://i.pinimg.com/originals/b7/ae/01/b7ae012431988c83c4c117375f2b1028.gif",
+//       reps: "12 Reps",
+//       info:"",
+//     },
+//     {
+//       id: 4,
+//       title: "Full Extension Kick-Back",
+//       img: "",
+//       gif:
+//         "https://3.bp.blogspot.com/-nMSgJq-WSBw/W7WrPZaJQWI/AAAAAAAAYTU/wYYCjfaydc04vazrqYt8xEA9JR1ujsQKQCKgBGAs/s1600/Kat-Forearm-Plank-Booty-Kickbacks.gif",
+//       reps: "15 Reps",
+//       info:"",
+//     },
+//     {
+//         id: 5,
+//         title: "Weighted Plie Lunge & Side Kick",
+//         img: "",
+//         gif:
+//           "https://imagesvc.meredithcorp.io/v3/mm/gif?q=85&c=sc&poi=face&w=900&h=900&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F14%2F2017%2F01%2F09%2F010517-tone-it-up-2.gif",
+//         reps: "15 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 6,
+//         title: "Straight Leg Hip Bridge",
+//         img: "",
+//         gif:
+//           "https://media.glamour.com/photos/586d34a63c6164143719aa85/master/w_1600%2Cc_limit/Kat-Single-Leg-Bridge-Dip_Tone_It_Up.gif",
+//         reps: "20 Reps (10 Per Side)",
+//         info:"",
+//       },
+//       {
+//         id: 7,
+//         title: "Weighted Reverse Lunge & Tricep Press",
+//         img: "",
+//         gif:
+//           "https://i.pinimg.com/originals/04/a3/0d/04a30d3847338ab76be264e455b5a544.gif",
+//         reps: "12 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 8,
+//         title: "Weighted Single Leg Dead Lift",
+//         img: "",
+//         gif:
+//           "https://1.bp.blogspot.com/-ij2S4ptCiDI/WscFsN1gPzI/AAAAAAAAK5E/Qklo8sRTvqwsAWiCcPjHWSvxWnlHaTJpQCEwYBhgL/s1600/Kat-DB-Single-Leg-Deadlift-2.gif",
+//         reps: "10 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 9,
+//         title: "Jump Squat",
+//         img: "",
+//         gif:
+//           "https://images.shape.mdpcdn.com/sites/shape.com/files/styles/slide/public/poppin-plyo-jumps-1-.gif",
+//         reps: "10 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 10,
+//         title: "Ab Bikes",
+//         img: "",
+//         gif:
+//           "https://i.pinimg.com/originals/a8/7c/d7/a87cd7734e82bdb5a264268a4afe1116.gif",
+//         reps: "15 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 11,
+//         title: "Plank & Kick-Back",
+//         img: "",
+//         gif:
+//           "https://bornrealist.com/wp-content/uploads/2017/10/Katrina-Single-Leg-Donkey-Kicks-CROPPED-1-_0.gif",
+//         reps: "20 Reps (10 Per Side)",
+//         info:"",
+//       },
+//       {
+//         id: 12,
+//         title: "Running Man Hip Bridge",
+//         img: "",
+//         gif:
+//           "https://4.bp.blogspot.com/-57S8RXhFbko/Wr3Y8qN00dI/AAAAAAAAKUo/MPzPj0H25d4oCnnIY0tq8tSMTyNzgDEWACEwYBhgL/s1600/Katrina-Bridge-March.gif",
+//         reps: "10 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 13,
+//         title: "Weighted Squat & Press",
+//         img: "",
+//         gif:
+//           "https://i.pinimg.com/originals/22/81/04/22810414c00749510ecf14782c5afb93.gif",
+//         reps: "15 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 14,
+//         title: "Lateral Lunge & High-Knees",
+//         img: "",
+//         gif:
+//           "http://cdn.lowgif.com/full/bc333564bb0f4a6b-.gif",
+//         reps: "20 Reps (10 Per Side)",
+//         info:"",
+//       },
+//       {
+//         id: 15,
+//         title: "Weighted Single Leg Dead Lift & Row",
+//         img: "",
+//         gif:
+//           "https://i.pinimg.com/originals/c2/ed/40/c2ed4017360c22036f461c16611d2f70.gif",
+//         reps: "12 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 16,
+//         title: "Weighted Reverse Lunge & Twist",
+//         img: "",
+//         gif:
+//           "https://1.bp.blogspot.com/-_ZeXLa5JiNA/W-fSiaTsuRI/AAAAAAAAZ9A/X107QVdjmeU9v_B0bAgXS0vpTLYsVMgIQCLcBGAs/s1600/Katrina-Kettle-Bell-Lunge-_-Twist-2.gif",
+//         reps: "10 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 17,
+//         title: "Weighted Reverse Lunge & Kick-Back",
+//         img: "",
+//         gif:
+//           "https://imagesvc.meredithcorp.io/v3/mm/gif?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2016%2F10%2F04034844%2Fskyword-image-352307.gif",
+//         reps: "12 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 18,
+//         title: "High Knees",
+//         img: "",
+//         gif:
+//           "https://www.byrdie.com/thmb/b7jVsmB-Yt-IIlWBEavA5O3mt9I=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__posts__239476__best-core-exercises-for-women-239476-1508796350476-main-bcdaaba08bec4724b9af22cf0787b149.gif",
+//         reps: "40 Reps (20 Per Side)",
+//         info:"",
+//       },
+//       {
+//         id: 19,
+//         title: "Weighted Squat",
+//         img: "",
+//         gif:
+//           "https://imagesvc.meredithcorp.io/v3/mm/gif?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2016%2F10%2F04034845%2Fskyword-image-352308.gif",
+//         reps: "12 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 20,
+//         title: "Weighted Russian Twist",
+//         img: "",
+//         gif:
+//           "https://www.byrdie.com/thmb/X6i-SN4uc7oh5OnJRcWW5EC_Ucs=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__posts__239476__best-core-exercises-for-women-239476-1508796422880-main-7e764e0e875a421c957e26a586849ab2.gif",
+//         reps: "30 Reps (15 Per Side)",
+//         info:"",
+//       },
+//       {
+//         id: 21,
+//         title: "Weighted Sumo Squat",
+//         img: "",
+//         gif:
+//           "https://3.bp.blogspot.com/-eWIAkgQhAcc/W77GcRQjDxI/AAAAAAAAYoQ/-7nAUlW-pF8_IRcWLB6KW_3EufLSotaVQCLcBGAs/s1600/Kat-Sumo-Squats-Square_d39a0021-4cbd-4e1c-8c7f-fc0a1fa6437d.gif",
+//         reps: "12 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 22,
+//         title: "Plank & Weighted Reverse Arm Swing",
+//         img: "",
+//         gif:
+//           "https://media1.popsugar-assets.com/files/thumbor/HvUHMLcG6FwrISbgQ_hXjwbKKGw/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/10/16/941/n/1922729/0038b22d3dc940e7_Kat-Plank-Tricep-Kickbacks-Square/i/Plank-Tricep-Kickbacks.gif",
+//         reps: "12 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 23,
+//         title: "Elevated Jump Squat",
+//         img: "",
+//         gif:
+//           "https://4.bp.blogspot.com/-mdPaxfzBP6o/WM6F1mkGauI/AAAAAAAAAIE/T_fZuQ9Z6WEkvj1IRYp_2otcI37uZDbRACLcB/s1600/10.gif",
+//         reps: "10 Reps",
+//         info:"",
+//       },
+//       {
+//         id: 24,
+//         title: "Half Burpee",
+//         img: "",
+//         gif:
+//           "https://www.byrdie.com/thmb/iHgVuvcsoVowTdLgx5WAthGIxB8=/700x700/filters:no_upscale():max_bytes(150000):strip_icc()/cdn.cliqueinc.com__posts__239476__best-core-exercises-for-women-239476-1508796471963-main-64af4fcda2a54eedbd7c395682c4fcd5.gif",
+//         reps: "15 Reps",
+//         info:"",
+//       }
+  
